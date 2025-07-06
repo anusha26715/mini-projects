f3207cfff43d6d92d1477605c86b330ebd79e4cc
anusha26715 <prathikantamanu@gmail.com>
2025-07-06 20:36:57 +0530

    Heart Project Updated

--- heart-project/app.js
+++ heart-project/app.js
@@ -1,12 +1,21 @@
 const greyHeart = document.querySelector('.grey-heart')
 const redHeart = document.querySelector('.red-heart')
+const msgEl = document.getElementById('message')
+const emgEl = document.getElementById('emoji')
 
 greyHeart.addEventListener("click",()=>{
     redHeart.classList.add('animation')
     greyHeart.classList.add('fill-color')
+    setTimeout(()=>{
+        msgEl.innerText = "I Fluffv u.."
+        emgEl.innerText = "😘"
+    },200)
+
 })
 
 redHeart.addEventListener("click",()=>{
     redHeart.classList.remove('animation')
     greyHeart.classList.remove('fill-color')
+    msgEl.innerText = "Click Heart"
+    emgEl.innerText = ''
 })
